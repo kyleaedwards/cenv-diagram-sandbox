3cd1cc65b3b39946790bad261a03bb0e97c8d401
Kyle Edwards <edwards.kyle.a@gmail.com>
2014-05-28 15:25:22 -0400

    update

--- client/index.php
+++ client/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>cEnv | Collaborative Diagram Prototyping Tool</title>
+        <title>flowboy | Collaborative Diagram Prototyping Tool</title>
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
         <meta name="robots" content="noindex, nofollow">
         <link rel="stylesheet" type="text/css" href="css/style.css" />
@@ -11,12 +11,13 @@
     </head>
     <body>
         <div id="header">
-            <h1>cEnv</h1>
+            <h1>flowboy</h1>
             <h2>Collaborative Diagram Prototyping Tool</h2>
         </div>
         <div id="contain">
             <canvas id="env-canvas"></canvas>
-            <div id="env-core"></div>
+            <div id="env-core">
+            </div>
             <div id="env-seed-options">
                 <h3 id="env-seed-name">Item Name</h3>
                     <p id="env-seed-description">Insert Description Here</p>
@@ -40,6 +41,13 @@
             <aside id="env-error-overlay">
                 <div id="env-error">Unable to connect to server. Please try again.</div>
             </aside>
+            <aside id="env-key-overlay">
+                <div id="env-key">
+                    Diagram Key: <input id="env-diagram-key" type="text" pattern="[a-zA-Z0-9-_ ]{6,25}" />
+                    <small>Type an existing key to load, type a new key to create a new diagram.</small>
+                </div>
+            </aside>
+            <aside id="env-info"></aside>
             <script src="js/script.js?cb=<?php for ($i = 0; $i < 8; $i++) echo rand(0, 9); ?>"></script>
         </div>
     </body>
